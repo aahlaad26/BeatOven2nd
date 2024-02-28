6ed78db94785610c8d33de3b587ecb94c5c0bc20
aahlaad26 <aahlaad26092002@gmail.com>
2024-02-28 16:29:33 +0530

    New Views

---
+++ BeatOven2nd/Model/Groups.swift
@@ -0,0 +1,8 @@
+//
+//  Groups.swift
+//  BeatOven2nd
+//
+//  Created by user2 on 28/02/24.
+//
+
+import Foundation
---
+++ BeatOven2nd/TrackLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackLibraryView.swift
+//  BeatOven2nd
+//
+//  Created by user2 on 28/02/24.
+//
+
+import SwiftUI
+
+struct TrackLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackLibraryView()
+}
