39e3b8187a559f84c441a3f35c7c019d7bcad280
aahlaad26 <aahlaad26092002@gmail.com>
2024-02-29 09:36:41 +0530

    Add plist

--- BeatOven2nd/App/BeatOven2ndApp.swift
+++ BeatOven2nd/App/BeatOven2ndApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct BeatOven2ndApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
