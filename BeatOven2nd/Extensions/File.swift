a9c7577983b7c42e9665680a2696e0821cd1ac70
aahlaad26 <aahlaad26092002@gmail.com>
2024-03-02 15:42:38 +0530

    tabBar color change

---
+++ BeatOven2nd/Extensions/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  BeatOven2nd
+//
+//  Created by user2 on 29/02/24.
+//
+
+import Foundation
---
+++ BeatOven2nd/Model/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BeatOven2nd
+//
+//  Created by user2 on 29/02/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
